05ceacf1ef2d4d1f767af2f8da73a6e874fcf091
Gautham Yerroju <gautham.yerroju@gmail.com>
2019-09-08 02:01:39 -0700

    attempt to improve error handling

--- app.js
+++ app.js
@@ -33,13 +33,22 @@ app.use(function(req, res, next) {
 
 // error handler
 app.use(function(err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  // TODO: detect if api or page, then choose res.render or res.json
+  if (false) {
+    res.status(err.status || 500).json({
+      code: err.code,
+      message: err.message,
+      stack: err.stack
+    })
+  } else {
+    // set locals, only providing error in development
+    res.locals.message = err.message;
+    res.locals.error = req.app.get('env') === 'development' ? err : {};
+  
+    // render the error page
+    res.status(err.status || 500);
+    res.render('error');
+  }
 });
 
 module.exports = app;
